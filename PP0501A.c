4675d014d6c593383f78858f08f0f258ebe7f332
szymon-tulodziecki <tulodziecki.kontakt@gmail.com>
2023-09-29 10:42:21 +0200

    spoj

---
+++ PP0501A.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+//https://pl.spoj.com/problems/PP0501A/
+
+void NWD(int x, int y)  // Eucalidean algorithm: https://en.wikipedia.org/wiki/Euclidean_algorithm
+{
+    if(x % y == 0)
+    {
+        printf("%d \n", y);
+    }
+    else
+    {
+        int hlp = x;
+        x = y;
+        y = hlp % y;
+
+        NWD(x, y);
+    }
+}
+//--------------------------------------------
+int main()
+{
+
+    int t;
+    scanf("%d", &t);
+
+    while(t--)
+    {
+        int a, b;
+
+        scanf("%d %d", &a, &b);
+        NWD(a, b);
+    }
+    
+    return 0;
+}
